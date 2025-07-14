62714d3bc9ca68b9cb8819d902e2b95a307032ab
OmSuryawanshi9011 <131766249+OmSuryawanshi9011@users.noreply.github.com>
2025-07-14 20:10:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 function insert_Row() {
     var table = document.getElementById("sampleTable");
 	var row = table.insertRow(0);
-	var cell1 = row.insertCelll(0);
-	var cell2 = row.insertCelll(1);
+	var cell1 = row.insertCell(0);
+	var cell2 = row.insertCell(1);
 	cell1.innerHTML = "New cell1";
 	cell2.innerHTML = "New cell2";
    
